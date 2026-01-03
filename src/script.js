8bb2666531b5f265fbedcb75bf561c0f4f4208d5
Faruque Abdullah <abdullahfaruque007@gmail.com>
2026-01-03 23:20:26 +0530

    result section added

--- src/script.js
+++ src/script.js
@@ -39,3 +39,30 @@ mobileNavs.forEach((e)=>{
 //     }
 //     menuIcon.src = isOpen ? closeIcon : hamburgerIcon;
 // });
+
+
+    // var swiper = new Swiper(".mySwiper", {
+    //   speed: 600,
+    //   parallax: true,
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //   },
+    //   navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    //   },
+    // });
+
+    var swiper = new Swiper(".mySwiper", {
+      cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+    });
